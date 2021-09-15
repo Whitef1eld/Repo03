b707bd6d4eb9d85c972a68971edbed250bbc7b6b
Whitef1eld <nwhitefiel1@etf.unsa.ba>
2021-09-15 15:19:24 +0200

    Added the index.js which does nothing and comments

---
+++ ex02/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    // Only change code below this line
+    
+    // Ovo je moj prvi komentar 
+
+    /* Ovo je moj drugi komentar, ali je malo
+    duzi jer koristim ovaj drugi metod */
+
+    // Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;
